912dca3ee72faaffa892de41ab2d82ea04196cf1
nancy nguyen <prancynguyen@gmail.com>
2017-12-09 20:37:23 -0800

    Modify products Route and Create Catalog File

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const products = require('../controllers/productsController');
+
+router.get('/', products.get);
+
 
 module.exports = router;
