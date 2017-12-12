d9971eb216e26a28f45b03d1c4ff38e74d0bec78
nancy nguyen <prancynguyen@gmail.com>
2017-12-12 11:30:55 -0800

    Display Categories Depending on NavBar

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -6,7 +6,8 @@ import { Link } from 'react-router-dom';
 const Products = (props) => {
   return (
     <div>
-        {props.products ? props.products.map((product, pIdx) => <li key={pIdx}>{product.name}</li>)
+        {props.products ?
+        <ul>{props.products.filter(product => product.product_type === props.category).map((product, pIdx) => <li key={pIdx}>{product.name}</li>)}</ul>
         : <h2>LOADING</h2>}
          {/* <Link to={`/products/${product.id}`}>{product.name}</Link></li>) */}
     </div>
--- src/pages/ProductIndexPage/ProductIndexPage.jsx
+++ src/pages/ProductIndexPage/ProductIndexPage.jsx
@@ -6,7 +6,7 @@ const ProductIndexPage = (props) => {
   return (
     <div>
       <br></br>ProductIndexPage<br></br><br></br>
-      <Catalog products={props.products}/>
+      <Catalog products={props.products} category={props.match.params.category}/>
     </div>
   )
 }
