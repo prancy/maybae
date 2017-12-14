5425647890450010d1e6a0a9710031cb7f5fa543
nancy nguyen <prancynguyen@gmail.com>
2017-12-13 16:23:50 -0800

    Added Cards in Product Index Page

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Card, CardTitle} from 'react-materialize';
+import {Card, CardTitle, Col, Row} from 'react-materialize';
 
 // props.product.filter(product) => product.product_type === :catalog in browser thingy
 
@@ -11,10 +11,13 @@ const Products = (props) => {
         //filter takes in a function and checks to see if product.product_type matches props.category, 
         //then .map method goes through each item in the array and changes it to however you want, //it changes depending on the function - for this instance
         // we are changing it to a list item where we list product.name
-        <ul>{props.products.filter(product => product.product_type === props.category).map((product, pIdx) => <li key={pIdx}><Link to={`/products/${product.id}`}>{product.name}<img src={product.image_link}/></Link><h2>{product.brand}</h2></li>)}</ul>
+        <Row>{props.products.filter(product => product.product_type === props.category).map((product, pIdx) => <Col m={4}><Card key={pIdx}>
+          <Link to={`/products/${product.id}`}>{product.name}<img src={product.image_link}/></Link></Card></Col>)}</Row>
         : <h2>LOADING</h2>}
     </div>
   )
 }
+
+
 
 export default Products;
