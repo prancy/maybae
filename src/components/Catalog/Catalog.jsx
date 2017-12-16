7059a93c5f115d26b74665ec2d9b159b28aeae23
nancy nguyen <prancynguyen@gmail.com>
2017-12-15 21:24:10 -0800

    Deleted comments and Fixed Indentation

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {Card, Col, Row} from 'react-materialize';
 
-// props.product.filter(product) => product.product_type === :catalog in browser thingy
-
 const Products = (props) => {
   return (
     <div className="productIndex">
     <br/>
-        {props.products.length > 0 ?
+      {props.products.length > 0 ?
         <Row>
           {props.products.filter(product => product.product_type === props.category).map((product, pIdx) => 
             <Col m={4}>
@@ -20,11 +18,14 @@ const Products = (props) => {
                 </div>
               </Card>
             </Col>)
-            }
+          }
         </Row>
         : 
-        <div><h2 className="center">We're Working on It!</h2><center><img src="https://i.imgur.com/8L2CwzP.gif" /></center></div>
-        }
+        <div>
+          <h2 className="center">We're Working on It!</h2>
+          <center><img src="https://i.imgur.com/8L2CwzP.gif" /></center>
+        </div>
+      }
     </div>
   )
 }
